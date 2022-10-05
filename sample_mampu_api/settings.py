473d1f06bd6ba60762d4a8e830aa4379ac95422c
Addin15 <addin.utb15@gmail.com>
2022-10-05 09:44:10 +0800

    update static url

--- sample_mampu_api/settings.py
+++ sample_mampu_api/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/home/Addin/sample-django-auth/static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
